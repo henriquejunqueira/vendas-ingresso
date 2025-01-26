d399d92e365a02efcefa9678af12fd738ec68dc7
henriquejunqueira <henriquejunqueira15@gmail.com>
2025-01-26 16:49:58 -0300

    Instaladas dependência do mysql, bcrypt e tipagens. Criado container docker com docker compose. Criados métodos POST e GET. Criada conexão com o container mysql.

--- README.md
+++ README.md
@@ -10,10 +10,33 @@
 - Instalação do typescript como dependência de desenvolvimento: `$ npm install typescript --save-dev`
 - Criação do arquivo tsconfig.json de configuração do typescript: `$ npx tsc --init`
 - Instalação do express: `$ npm install express`
-- Instalação da dependência de tipagens do typescript em modo de desenvolvimento: `$ npm install --save-dev @types/express`
+- Instalação da dependência de tipagens do typescript para o express em modo de desenvolvimento: `$ npm install --save-dev @types/express`
 - Instalação do ts-node como dependência de desenvolvimento: `$ npm install ts-node --save-dev`
-- Instalação nodemon como dependência de desenvolvimento: `$ npm i nodemon --save-dev`
+- Instalação nodemon como dependência de desenvolvimento: `$ npm install nodemon --save-dev`
+- Instalação do mysql2: `$ npm install mysql2`
+- Instalação do bcrypt: `$ npm install bcrypt`
+- Instalação das tipagens do typescript para o bcrypt como dependência de desenvolvimento: `$ npm install --save-dev @types/bcrypt`
 
 ### Rodando projeto
 
 - Rodando projeto utilizando nodemon: `$ npx nodemon`
+
+### Arquivo de configuração do container docker:
+
+- Configurado arquivo docker-composer.yaml na raiz do projeto
+
+### Levantando o container docker
+
+- Rodando docker compose: `$ docker compose up`
+
+### Remove o container docker
+
+- Removendo o container: `$ docker compose down`
+
+### Visualizando os containers docker rodando
+
+- Listando container ativos: `$ docker ps`
+
+### Parando o container docker
+
+- Stopando o container pelo id: `$ $ docker stop numero_id_container`
--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,83 @@
 import express from 'express';
+import * as mysql from 'mysql2/promise';
+import bcrypt from 'bcrypt';
+
+function createConnection() {
+  return mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'root',
+    database: 'tickets',
+    port: 33060,
+  });
+}
 
 const app = express();
 
+app.use(express.json());
+
 app.get('/', (req, res) => {
   res.json({ message: 'Hello World!' });
+});
+
+app.post('/auth/login', (req, res) => {
+  const { email, password } = req.body;
+  console.log(email, password);
+  res.send();
+});
+
+app.post('/partners', async (req, res) => {
+  const { name, email, password, company_name } = req.body;
+
+  const connection = await createConnection();
+  try {
+    const createdAt = new Date();
+    const hashedPassword = bcrypt.hashSync(password, 10); // 10 é a força da senha
+
+    const [userResult] = await connection.execute<mysql.ResultSetHeader>(
+      'INSERT INTO users (name, email, password, created_at) VALUES (?, ?, ?, ?)',
+      [name, email, hashedPassword, createdAt]
+    );
+
+    const userId = userResult.insertId;
+    const [partnerResult] = await connection.execute<mysql.ResultSetHeader>(
+      'INSERT INTO partners (user_id, company_name, created_at) VALUES (?, ?, ?)',
+      [userId, company_name, createdAt]
+    );
+
+    res.status(201).json({
+      id: partnerResult.insertId,
+      user_id: userId,
+      company_name,
+      created_at: createdAt,
+    });
+  } finally {
+    await connection.end();
+  }
+});
+
+app.post('/customers', (req, res) => {
+  const { name, email, password, address, phone } = req.body;
+});
+
+app.post('/partners/events', (req, res) => {
+  const { name, description, date, location } = req.body;
+});
+
+app.get('/partners/events', (req, res) => {});
+
+app.get('/partners/events/:eventId', (req, res) => {
+  const { eventId } = req.params;
+  console.log(eventId);
+  res.send();
+});
+
+app.get('/events', (req, res) => {});
+
+app.get('/events/:eventId', (req, res) => {
+  const { eventId } = req.params;
+  console.log(eventId);
+  res.send();
 });
 
 app.listen(3000, () => {
