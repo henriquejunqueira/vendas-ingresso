71c6d340b9309abe750f6eff58e54a3dee528229
henriquejunqueira <henriquejunqueira15@gmail.com>
2025-01-28 16:57:13 -0300

    Configurando autenticação com JWT e testando

--- src/app.ts
+++ src/app.ts
@@ -17,6 +17,54 @@ const app = express();
 
 app.use(express.json());
 
+// mapeia o que não precisa ser protegido com token jwt
+const unprotectedRoutes = [
+  { method: 'POST', path: '/auth/login' },
+  { method: 'POST', path: '/customers/register' },
+  { method: 'POST', path: '/partners/register' },
+  { method: 'GET', path: '/events' },
+];
+
+// Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiZW1haWwiOiJwYXJ0bmVyMUB1c2VyLmNvbSIsImlhdCI6MTczNzEyOTkyMCwiZXhwIjoxNzM3MTMzNTIwfQ.Mshhxlgy34pAu_9Wn9fbNaCznC9Un3E-JX5p_0o-dkM
+
+app.use(async (req, res, next) => {
+  const isUnprotectedRoute = unprotectedRoutes.some(
+    (route) => route.method == req.method && req.path.startsWith(route.path)
+  );
+
+  if (isUnprotectedRoute) {
+    return next();
+  }
+
+  const token = req.headers['authorization']?.split(' ')[1];
+  if (!token) {
+    res.status(401).json({ message: 'No token provided' });
+
+    return;
+  }
+
+  try {
+    const payload = jwt.verify(token, '123456') as {
+      id: number;
+      email: string;
+    };
+    const connection = await createConnection();
+    const [rows] = await connection.execute<mysql.RowDataPacket[]>(
+      'SELECT * FROM users WHERE id = ?',
+      [payload.id]
+    );
+    const user = rows.length ? rows[0] : null;
+    if (!user) {
+      res.status(401).json({ message: 'Failed to authenticate token' });
+      return;
+    }
+    req.user = user as { id: number; email: string };
+    next();
+  } catch (error) {
+    res.status(401).json({ message: 'Failed to authenticate token' });
+  }
+});
+
 app.get('/', (req, res) => {
   res.json({ message: 'Hello World!' });
 });
@@ -51,7 +99,7 @@ app.post('/auth/login', async (req, res) => {
   res.send();
 });
 
-app.post('/partners', async (req, res) => {
+app.post('/partners/register', async (req, res) => {
   const { name, email, password, company_name } = req.body;
 
   const connection = await createConnection();
@@ -82,7 +130,7 @@ app.post('/partners', async (req, res) => {
   }
 });
 
-app.post('/customers', async (req, res) => {
+app.post('/customers/register', async (req, res) => {
   const { name, email, password, address, phone } = req.body;
 
   const connection = await createConnection();
@@ -114,8 +162,44 @@ app.post('/customers', async (req, res) => {
   }
 });
 
-app.post('/partners/events', (req, res) => {
+app.post('/partners/events', async (req, res) => {
   const { name, description, date, location } = req.body;
+  const userId = req.user!.id;
+  const connection = await createConnection();
+
+  try {
+    const [rows] = await connection.execute<mysql.RowDataPacket[]>(
+      'SELECT * FROM partners WHERE user_id = ?',
+      [userId]
+    );
+
+    const partner = rows.length ? rows[0] : null;
+
+    if (!partner) {
+      res.status(403).json({ message: 'Not authorized' });
+      return;
+    }
+
+    const eventDate = new Date(date);
+    const createdAt = new Date();
+
+    const [eventResult] = await connection.execute<mysql.ResultSetHeader>(
+      'INSERT INTO events (name, description, date, location, created_at, partner_id) VALUES (?, ?, ?, ?, ?, ?)',
+      [name, description, eventDate, location, createdAt, partner.id]
+    );
+
+    res.status(201).json({
+      id: eventResult.insertId,
+      name,
+      description,
+      date: eventDate,
+      location,
+      created_at: createdAt,
+      partner_id: partner.id,
+    });
+  } finally {
+    await connection.end();
+  }
 });
 
 app.get('/partners/events', (req, res) => {});
